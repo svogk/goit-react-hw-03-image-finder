99fd4105c0d904f044c2d6c80525c32338ea0cb7
Svitlana Bondarenko <svogk@ua.fm>
2021-01-05 12:15:22 +0200

    edit apiService

--- src/apiService/apiService.js
+++ src/apiService/apiService.js
@@ -1,8 +1,9 @@
 const apiKey = '16340198-dc2f265d0add10e2fffcbdf49';
-const apiUrl = 'https://pixabay.com/api';
+const apiUrl = '//pixabay.com/api';
 
 function fetchHits(searchQuery, page) {
   const url = `${apiUrl}?q=${searchQuery}&page=${page}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=12`;
+
   return fetch(url).then(response => response.json());
 }
 
